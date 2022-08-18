d428a3fc986da82628fef3516531c8372c6283ac
liverelay <liverelaytv@gmail.com>
2022-08-18 09:19:35 +0800

    modify hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.10",
   networks: {
@@ -9,4 +11,7 @@ module.exports = {
       accounts: [process.env.PRIVATE_KEY]
     },
   },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  }
 };
